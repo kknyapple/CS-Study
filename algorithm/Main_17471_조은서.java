ba27cb57cd6ae361d960fda4269afffca2956379
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2023-08-30 22:11:01 +0900

    Create Main_17471_조은서.java

---
+++ algorithm/Main_17471_조은서.java
@@ -0,0 +1,10 @@
+public class Main_17471_조은서 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+
+		N = Integer.parseInt(br.readLine());
+		
+	}
+}
